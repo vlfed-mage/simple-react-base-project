8fa9004eb04fbee52f88526316bd7b86e9af00a0
Valdo <gkorchun@gmail.com>
2023-02-09 11:24:30 +0200

    Passing different swiper params

---
+++ src/dev/swiper-params/swiper-params.js
@@ -0,0 +1,66 @@
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper';
+
+const swiperParams = {
+    people: {
+        modules: [Navigation, Pagination, Scrollbar, A11y],
+        breakpoints: {
+            640: {
+                slidesPerView: 3,
+            },
+            1024: {
+                slidesPerView: 4,
+            },
+            1440: {
+                slidesPerView: 5,
+            },
+        },
+        spaceBetween: 16,
+        slidesPerView: 2,
+        navigation: {},
+        pagination: { clickable: true },
+        scrollbar: { draggable: true },
+        onSwiper: (swiper) => console.log(swiper),
+        onSlideChange: () => console.log(`slide change`)
+    },
+    planets: {
+        modules: [Navigation, Pagination, Scrollbar, A11y],
+        breakpoints: {
+            640: {
+                slidesPerView: 3,
+            },
+            1024: {
+                slidesPerView: 4,
+            }
+        },
+        spaceBetween: 16,
+        slidesPerView: 2,
+        navigation: {},
+        pagination: { clickable: true },
+        scrollbar: { draggable: true },
+        onSwiper: (swiper) => console.log(swiper),
+        onSlideChange: () => console.log(`slide change`)
+    },
+    starships: {
+        modules: [Navigation, Pagination, Scrollbar, A11y],
+        breakpoints: {
+            640: {
+                slidesPerView: 3,
+            },
+            1024: {
+                slidesPerView: 4,
+            },
+            1440: {
+                slidesPerView: 6,
+            },
+        },
+        spaceBetween: 16,
+        slidesPerView: 2,
+        navigation: {},
+        pagination: { clickable: true },
+        scrollbar: { draggable: true },
+        onSwiper: (swiper) => console.log(swiper),
+        onSlideChange: () => console.log(`slide change`)
+    },
+};
+
+export default swiperParams;

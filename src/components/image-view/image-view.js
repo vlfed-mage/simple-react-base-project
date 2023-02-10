8edd7ccb1eeb3b6645209efaa84acd95e007e4d5
Valdo <gkorchun@gmail.com>
2023-02-10 12:42:48 +0200

    Set home page

--- src/components/image-view/image-view.js
+++ src/components/image-view/image-view.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-import { getImgUrl, onImageError } from '../helpers';
-
 const ImageView = ({ item, category }) => {
-    const imageName = `${ item.name }-image`;
+    const imageName = `${ item.name }-image`,
+    _bodyImgUrl = 'https://starwars-visualguide.com/assets/img/',
+
+    _extractId = (url) => {
+        const regex = /\/([0-9]*)\/$/;
+        return url.match(regex)[1];
+    },
+
+    getImgUrl = (item, category) => {
+        const id = _extractId(item.url)
+        const group = category === 'people' ? 'characters' : category;
+        return `${_bodyImgUrl}${group}/${id}.jpg`
+    },
+
+    onImageError = (e) => {
+        e.target.src = require('../../images/dumb-img.svg');
+    };
 
     return (
         <img src={ getImgUrl(item, category) }
---
+++ src/components/pages/home-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Slider from '../slider';
+
+const HomePage = () => {
+    return (
+        <>
+            <Slider category='people' />
+            <Slider category='planets' />
+            <Slider category='starships' />
+        </>
+    );
+};
+
+export default HomePage;
